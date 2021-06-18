ddaf3df577696ce3135646510d8bf307d414338d
Maxime HERY <maximehery18@outlook.fr>
2021-06-18 20:37:39 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+
+      primary: {
+        100: '#53C351',
+        200: '#1C3A27'
+      },
+      secondary: {
+        100: '#E2EEEC',
+        200: '#254330',
+        300: '#F4F9F8'
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
